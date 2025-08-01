9c3e3e7f872b4029e5536dcb0c68ce245014c079
SenhasD <senhasdiwan@gmail.com>
2025-08-02 01:41:52 +0530

    Player dialogues and npc

---
+++ timelooped/Characters/guide/guide.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var balloon_scene = preload("res://dialog/game_dialuge_balloon.tscn")
+
+@onready var interactable_component: Interactable = $Interactable
+@onready var interactable_label_component: Control = $InteractableLabelComponent
+
+var in_range: bool
+
+func _ready() -> void:
+	interactable_component.interactable_activated.connect(on_interactable_activated)
+	interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
+	interactable_label_component.hide()
+	
+func on_interactable_activated() -> void:
+	interactable_label_component.show()
+	in_range = true
+		
+func on_interactable_deactivated() -> void:
+	interactable_label_component.hide()
+	in_range = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if in_range:
+		if event.is_action_pressed("show_dialogue"):
+			var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+			get_tree().current_scene.add_child(balloon)
+			balloon.start(load("res://dialog/story/guide.dialogue"), "start")
