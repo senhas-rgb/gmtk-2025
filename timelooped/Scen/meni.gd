d79ceb70306b2da69db3d698b1126ff56fe71923
Anjo1920 <anjodima54@gmail.com>
2025-08-01 14:55:18 +0200

    credits :fire:

--- timelooped/Scen/meni.gd
+++ timelooped/Scen/meni.gd
@@ -21,7 +21,7 @@ func _on_play_pressed() -> void:
 	
 
 func _on_credit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scen/credits.tscn")
 
 
 func _on_setting_pressed() -> void:
