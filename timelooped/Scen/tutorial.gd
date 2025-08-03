3e1b2eb50d3494c945d9365fb0587d312ded3714
Nugetinc <nugetinc@gmail.com>
2025-08-03 13:44:57 +1000

    so close to being done now

---
+++ timelooped/Scen/tutorial.gd
@@ -0,0 +1,10 @@
+extends ColorRect
+
+@onready var button: Button = $Button
+@onready var music: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+func _ready() -> void:
+	music.play()
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/house.tscn")
