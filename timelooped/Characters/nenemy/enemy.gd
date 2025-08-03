6ab3143217a6ef319b59c6e2f269cfe42757278d
Anjo1920 <anjodima54@gmail.com>
2025-08-03 12:16:41 +0200

    dialog is now attached to the enemy :fire:

--- timelooped/Characters/nenemy/enemy.gd
+++ timelooped/Characters/nenemy/enemy.gd
@@ -2,12 +2,17 @@ extends CharacterBody2D
 
 @export var speed := 40
 @export var patrol_distance := 100
+@onready var getkilledzone = $KillEnemyZone
+var balloon_scene = preload("res://dialog/game_dialuge_balloon.tscn")
 
 var start_position: Vector2
 var moving_right := true
 
 func _ready():
 	start_position = position
+	getkilledzone.body_entered.connect(_on_player_near)
+	getkilledzone.body_exited.connect(_on_player_far)
+	
 
 func _physics_process(delta):
 	var direction = Vector2.RIGHT if moving_right else Vector2.LEFT
@@ -24,3 +29,16 @@ func _on_hurt_zone_body_entered(body: Node2D) -> void:
 
 	if body is Player:
 		body.hurt()
+
+
+
+func _on_player_near(body) -> void:
+	if body.name == "Player":
+		print("Player is near the apple")
+		var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+		get_tree().current_scene.add_child(balloon)
+		balloon.start(load("res://dialog/story/player_see_apple.dialogue"), "start")
+
+func _on_player_far(body):
+	if body.name == "Player":
+		print("Player walked away from the apple")
