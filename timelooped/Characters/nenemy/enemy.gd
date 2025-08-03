905449ea790d2d5136e3a4d646360f9471efd680
SenhasD <senhasdiwan@gmail.com>
2025-08-03 17:42:40 +0530

    fix this

--- timelooped/Characters/nenemy/enemy.gd
+++ timelooped/Characters/nenemy/enemy.gd
@@ -5,12 +5,20 @@ var balloon_scene = preload("res://dialog/game_dialuge_balloon.tscn")
 @onready var interactable_component: Interactable = $Interactable
 @onready var interactable_label_component: Control = $InteractableLabelComponent
 @onready var enemy: CharacterBody2D = $"."
-var in_range: bool
+@onready var collision: CollisionShape2D = $Interactable/CollisionShape2D
+@onready var collision2: CollisionShape2D = $HurtZone/CollisionShape2D
+@onready var in_range: bool
+@onready var killed: bool = false;
 
 func _ready() -> void:
 	interactable_component.interactable_activated.connect(on_interactable_activated)
 	interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
 	interactable_label_component.hide()
+	
+	if killed == true:
+		queue_free()
+		collision.disabled = true
+		collision2.disabled = true
 	
 func on_interactable_activated() -> void:
 	interactable_label_component.show()
@@ -19,7 +27,10 @@ func on_interactable_activated() -> void:
 func on_interactable_deactivated() -> void:
 	interactable_label_component.hide()
 	in_range = false
+	killed = true
 	queue_free()
+	collision.disabled = true
+	collision2.disabled = true
 
 func _unhandled_input(event: InputEvent) -> void:
 	if in_range:
