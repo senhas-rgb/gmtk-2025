c18a0867c394149fca9001865a36cc72c86c955c
Nugetinc <nugetinc@gmail.com>
2025-08-20 18:11:11 +1000

    gamer menu

--- timelooped/Scen/meni.gd
+++ timelooped/Scen/meni.gd
@@ -1,17 +1,29 @@
 extends Control
 
-@onready var play: Button = $VBoxContainer/play
-@onready var credit: Button = $VBoxContainer/credit
+@onready var play: Button = $play
+@onready var credit: Button = $credit
+@onready var buttons_parent: Control = $"."
 @onready var splash_text: RichTextLabel = $"Splash Text"
+@onready var red_bg: AnimatedSprite2D = $AngledRed
+@onready var green_bg: AnimatedSprite2D = $AngledGreen
+
 
 var os_name = OS.get_name()
 var splash = {}
+var themes = {
+	"play": preload("res://Art/Menu/red.tres"),
+	"credit": preload("res://Art/Menu/green.tres")
+}
 
 func _ready():
 	randomize()
 	print(os_name)
 	load_splash_data()
 	random_splash()
+	
+	play.connect("mouse_entered", Callable(self, "_on_button_hovered").bind("play"))
+	credit.connect("mouse_entered", Callable(self, "_on_button_hovered").bind("credit"))
+
 
 func _on_resume_button_pressed():
 	get_tree().paused = false
@@ -38,3 +50,23 @@ func random_splash():
 	var messages = splash[key]
 	if messages.size() > 0:
 		splash_text.text = messages[randi() % messages.size()]
+
+
+func _on_button_hovered(theme_name: String):
+	if not themes.has(theme_name):
+		return
+	
+	var tween := create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT).set_parallel(true)
+	
+	match theme_name:
+		"play":
+			tween.tween_property(red_bg, "modulate:a", 1.0, 0.2)
+			tween.tween_property(green_bg, "modulate:a", 0.0, 0.2)
+		"credit":
+			tween.tween_property(red_bg, "modulate:a", 0.0, 0.2)
+			tween.tween_property(green_bg, "modulate:a", 1.0, 0.2)
+
+	var new_theme = themes[theme_name]
+	for button in buttons_parent.get_children():
+		if button is Button:
+			button.theme = new_theme
